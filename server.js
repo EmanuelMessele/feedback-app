21d580db730d5f9b9e548022d5230bd9ce7e3b58
EmanuelMessele <emanuelmessele1@gmail.com>
2025-06-25 13:38:19 -0400

    i added a POST ability allowing users to input their data, no front end to do it with, so I tested it using curl command, not sure if I want to add react front end right now, im really trying to focus on node, but i might just to get my understanding of react fresh. learned a lot today

--- server.js
+++ server.js
@@ -20,11 +20,47 @@ const server = http.createServer((req, res) => { // callback function
    const parsedUrl = url.parse(req.url, true); // true just means yes, we will parse the info into an object
 
    // working with GET  --> obtaining from the server
-    if (req.method == 'GET' && parsedUrl.pathname == 'submit'){ // req.method is asking what kind of request are we dealing with
+    if (req.method == 'POST' && parsedUrl.pathname == '/submit'){ // req.method is asking what kind of request are we dealing with
+
+        body = '';
+        // we must collect data from the request body
+        req.on('data', chunk => {
+            body += chunk.toString(); // since chunk is a raw binary object we have to toString it
+        })
+
+        // now we must parse the data, when done reading
+        req.on('end', () =>{
+        try{
+          const data = JSON.parse(body); // parse our body 
+          const content = `Name: ${data.name} Message: ${data.message}`;
+
+          fs.writeFile('feedback.txt', content, 'utf-8', (err) =>{
+            if(err){
+                console.error("File apending error")
+                return; 
+            }
+                res.writeHead('200', {'Content-Type': 'text/plain'}); 
+                res.end("Feedback saved")
+          });
+
+        } catch(err) {
+            console.error("invalid:" + err);
+            res.writeHead('200', {'Content-Type': 'text/plain' });
+            res.end("Feedback Not received");
+        }
+        });
+        
+
+        
+
+        // respond to the user
+
 
 
         // working with POST --> sending to the server
-    } else if(req.method == 'POST' && parsedUrl.pathname == 'submit'){ // parsedUrl.pathname checks the pathname in our parsedUrl object
+    } else if(req.method == 'GET' && parsedUrl.pathname == '/feedbacks'){ // parsedUrl.pathname checks the pathname in our parsedUrl object
+
+
 
     } else {
             // then the route is invalid, send an error
@@ -35,5 +71,5 @@ const server = http.createServer((req, res) => { // callback function
 // this tells node to start listening for requests
 // runs the callback, console log once server is ready
 server.listen(PORT, 'localhost', () => {
-    console.log("Server running on" + PORT)
+    console.log("Server running on " + PORT)
 })
